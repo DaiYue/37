21808de1c90711ad696dc678cdf1414e7ebd7cc1
jh <stjh10@gmail.com>
2014-03-03 20:09:09 +0800

    move logger to config

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.configure(function() {
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
   app.use(express.favicon());
-  app.use(express.logger('dev'));
+  app.use(express.logger(config.logger));
   app.use(express.json());
   app.use(express.urlencoded());
   app.use(express.methodOverride());
